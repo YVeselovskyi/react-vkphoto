7b2ca19afcf90285119248180aa9e6d7a66b4ac5
Yaroslav Veselovskyi <yveselovskyi@MBP-Yaroslav.Dlink>
2016-12-26 21:51:09 +0200

    Env port

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const app = express();
-
+const PORT = 3000 || process.env.PORT;
 app.use(express.static('public'));
 
 app.use(express.static('./node_modules/bootstrap/dist'));
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
 	console.log('Success');
 });
