27ce20e02284d7d179ece26e971d79fd9270b703
Yaroslav Veselovskyi <yveselovskyi@MBP-Yaroslav.Dlink>
2016-12-26 21:59:53 +0200

    Server fixes

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
-app.use(express.static('public'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/public/index.html');
+});
 
 app.use(express.static('./node_modules/bootstrap/dist'));
 
